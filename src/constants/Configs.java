de05e1e40f711ec04dc5c38e85ddbbecfe6895a6
Klrushka <comeindonotbeshy@gmail.com>
2021-09-05 18:09:21 +0300

    Initial commit

---
+++ src/constants/Configs.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class Configs {
+    protected String dbHost = "KlrushkaDB";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "SQL2021";
+    protected String dbName = "eyeofgod";
+}
---
+++ src/constants/FieldNames.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class FieldNames {
+    public static final String USER_ID = "idpeople";
+    public static final String FIRSTNAME = "firstname";
+    public static final String SECONDNAME = "secondname";
+    public static final String USERNAME = "idpeople";
+    public static final String BIRTHDAY = "idpeople";
+
+}
